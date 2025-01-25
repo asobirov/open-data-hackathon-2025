4f3dd059a67ff544f053f946076f522a52f4e0b6
Akbarshokh Sobirov <asobirov357@gmail.com>
2025-01-25 19:04:23 +0500

    feat: enhance sidebar layout and styling

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,14 +3,21 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
 import { cn } from "@duck/ui";
-import { SidebarProvider } from "@duck/ui/sidebar";
-import { ThemeProvider } from "@duck/ui/theme";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@duck/ui/sidebar";
+import { ThemeProvider, ThemeToggle } from "@duck/ui/theme";
 import { Toaster } from "@duck/ui/toast";
 
 import { TRPCReactProvider } from "@/trpc/react";
 
 import "@/app/globals.css";
 
+import { Separator } from "@duck/ui/separator";
+
+import { AppSidebar } from "@/app/_components/side-bar";
 import { env } from "@/env";
 
 export const metadata: Metadata = {
@@ -53,7 +60,21 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <TRPCReactProvider>
-            <SidebarProvider>{props.children}</SidebarProvider>
+            <SidebarProvider>
+              <>
+                <AppSidebar />
+                <SidebarInset>
+                  <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <div className="flex flex-1 items-center justify-end">
+                      <ThemeToggle />
+                    </div>
+                  </header>
+                  <main className="px-4">{props.children}</main>
+                </SidebarInset>
+              </>
+            </SidebarProvider>
           </TRPCReactProvider>
           <Toaster />
         </ThemeProvider>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,26 +1,9 @@
-import { Separator } from "@duck/ui/separator";
-import { SidebarInset, SidebarTrigger } from "@duck/ui/sidebar";
-import { ThemeToggle } from "@duck/ui/theme";
-
 import { CompanyTable } from "./_components/company-table";
-import { AppSidebar } from "./_components/side-bar";
 
 export default function Page() {
   return (
-    <>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <div className="flex flex-1 items-center justify-end">
-            <ThemeToggle />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <CompanyTable />
-        </div>
-      </SidebarInset>
-    </>
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <CompanyTable />
+    </div>
   );
 }
